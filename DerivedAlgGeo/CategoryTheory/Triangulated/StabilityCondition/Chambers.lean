ce50c87639d78a9def07d2c39dde16c4f83227fb
Chris Dare <cedare96@gmail.com>
2026-09-11 22:06:01 -0400

    Define Stab-level walls and chambers

---
+++ DerivedAlgGeo/CategoryTheory/Triangulated/StabilityCondition/Chambers.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2026 Chris Dare. All rights reserved.
+Released under the MIT license.
+-/
+import DerivedAlgGeo.CategoryTheory.Triangulated.StabilityCondition.Chambers.Basic
+
+/-!
+# Chambers in the stability space
+
+Charge-vanishing walls, their regular loci, and connected components of those
+regular loci.
+-/
