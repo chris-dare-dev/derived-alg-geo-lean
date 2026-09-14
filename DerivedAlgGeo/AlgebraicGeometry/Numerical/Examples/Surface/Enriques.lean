54acc12fb013cfa6d2c73629a4f9d0980ca42410
Chris Dare <cedare96@gmail.com>
2026-09-14 17:28:29 -0400

    Import the neutral surface specialization directly

--- DerivedAlgGeo/AlgebraicGeometry/Numerical/Examples/Surface/Enriques.lean
+++ DerivedAlgGeo/AlgebraicGeometry/Numerical/Examples/Surface/Enriques.lean
@@ -4,6 +4,7 @@ Released under the MIT license.
 -/
 import DerivedAlgGeo.AlgebraicGeometry.Numerical.Examples.Surface.RankOne
 import DerivedAlgGeo.AlgebraicGeometry.Numerical.RiemannRoch.Enriques
+import DerivedAlgGeo.AlgebraicGeometry.Numerical.Specializations.Surface
 
 /-!
 # A numerical Enriques surface of Picard rank one
