4f9b31f34b8781c89ff18697523f2b91408ddb1a
Chris Dare <cedare96@gmail.com>
2026-09-24 17:32:37 -0400

    SF11.4bf: patch finite submodules across two principal localizations

--- DerivedAlgGeo/Algebra/Module/LocalizedModule.lean
+++ DerivedAlgGeo/Algebra/Module/LocalizedModule.lean
@@ -3,5 +3,6 @@ Copyright (c) 2026 Chris Dare. All rights reserved.
 Released under the MIT license.
 -/
 import DerivedAlgGeo.Algebra.Module.LocalizedModule.Submodule
+import DerivedAlgGeo.Algebra.Module.LocalizedModule.TwoChartSubmodule
 
 /-! # Localized modules -/
