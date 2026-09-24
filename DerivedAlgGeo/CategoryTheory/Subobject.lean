432db45608f1f003233417c0087ed10406b851c2
Chris Dare <cedare96@gmail.com>
2026-09-24 19:25:21 -0400

    Prove coherent subobject order is local on open covers

--- DerivedAlgGeo/CategoryTheory/Subobject.lean
+++ DerivedAlgGeo/CategoryTheory/Subobject.lean
@@ -1,4 +1,5 @@
 import DerivedAlgGeo.CategoryTheory.Subobject.NoetherianObject
 import DerivedAlgGeo.CategoryTheory.Subobject.LocalizedHomArrowExtension
+import DerivedAlgGeo.CategoryTheory.Subobject.JointReflection
 
 /-! # Subobjects -/
