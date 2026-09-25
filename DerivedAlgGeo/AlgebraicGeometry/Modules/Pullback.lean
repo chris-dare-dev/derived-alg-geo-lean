5c632cab92ebdbdd6e87a60eb240b6bf431ef2bf
Chris Dare <cedare96@gmail.com>
2026-09-24 22:12:17 -0400

    feat: glue scalar-extended pullback units across two opens

--- DerivedAlgGeo/AlgebraicGeometry/Modules/Pullback.lean
+++ DerivedAlgGeo/AlgebraicGeometry/Modules/Pullback.lean
@@ -8,6 +8,7 @@ import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.Monoidal
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.Restriction
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.ScalarExtendedSections
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.ScalarExtendedOpenSections
+import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.ScalarExtendedTwoOpen
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.Stalk
 
 /-! # Pullback of scheme-module sheaves -/
