c94bda72f86edbbf219db3ed01ef9e31747807cb
Chris Dare <cedare96@gmail.com>
2026-09-24 21:22:37 -0400

    feat: transpose actual pullback sections through scalar extension

--- DerivedAlgGeo/AlgebraicGeometry/Modules/Pullback.lean
+++ DerivedAlgGeo/AlgebraicGeometry/Modules/Pullback.lean
@@ -4,6 +4,7 @@ import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.FixedBaseSections
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.LocallyFree
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.Monoidal
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.Restriction
+import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.ScalarExtendedSections
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback.Stalk
 
 /-! # Pullback of scheme-module sheaves -/
