214e1530adf9be6497a8b3705a2e21d6e56ebe52
Chris Dare <cedare96@gmail.com>
2026-09-24 21:22:31 -0400

    feat: localize fixed-base two-open section limits

--- DerivedAlgGeo/AlgebraicGeometry/Modules.lean
+++ DerivedAlgGeo/AlgebraicGeometry/Modules.lean
@@ -7,6 +7,7 @@ import DerivedAlgGeo.AlgebraicGeometry.Modules.Flasque
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Flat
 import DerivedAlgGeo.AlgebraicGeometry.Modules.FlatGenerators
 import DerivedAlgGeo.AlgebraicGeometry.Modules.FixedBaseSheaf
+import DerivedAlgGeo.AlgebraicGeometry.Modules.FixedBaseSheafLocalization
 import DerivedAlgGeo.AlgebraicGeometry.Modules.LocalCombination
 import DerivedAlgGeo.AlgebraicGeometry.Modules.LocallySurjective
 import DerivedAlgGeo.AlgebraicGeometry.Modules.Pullback
